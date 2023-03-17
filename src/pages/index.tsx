e76adf5f82f39e4c0514ff4b7b24d43e748f8e7e
ranjabi <fikriranjabi02@gmail.com>
2023-03-17 13:41:44 +0700

    feat: init next typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fikri Ranjabi</title>
+        <meta name="description" content="Fikri Ranjabi Personal Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main><p>hola!</p></main>
+    </>
+  )
+}
