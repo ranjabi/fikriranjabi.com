fbd2c25322150d408fd2b283a801f22a2d63be8a
ranjabi <fikriranjabi02@gmail.com>
2023-03-27 16:18:04 +0700

    style: hero animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,13 @@
 import { Divider, Flex, Text, useBreakpointValue } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 const dynamicText = ["Fikri Ranjabi", "Software Engineer", "Comp Sci Student"];
 
 const Hero = ({}) => {
-  const isLineBreak = useBreakpointValue({ base: true, sm: true, md: false });
+  const isLineBreak = useBreakpointValue({ base: true, md: false });
   const [position, setPosition] = useState(0);
+
   useEffect(() => {
     const interval = setInterval(() => {
       setPosition((position + 1) % dynamicText.length);
@@ -13,17 +15,44 @@ const Hero = ({}) => {
     return () => clearInterval(interval);
   }, [position]);
 
+  const variants = {
+    visible: {
+      opacity: 1,
+      transition: {},
+    },
+    hidden: {
+      opacity: 0,
+      transition: {},
+    },
+  };
+
   return (
     <>
-    <Flex flexDirection={"column"} py={[0, 4, 8]} mb="4">
-      <Text fontSize={["4xl", "5xl"]} fontWeight={"bold"} color="blackAlpha.500">
-        Hello, I'm {isLineBreak ? <br /> : " "}
-        <Text as={"span"} fontSize={["4xl", "5xl"]} color="black">
-          {dynamicText[position]}
-        </Text>
-      </Text>
-    </Flex>
-      <Divider/>
+      <Flex flexDirection={"column"} py={[0, 4, 8]} mb="4">
+        <Flex flexDirection={isLineBreak ? 'column' : 'row'}>
+          <Text
+            fontSize={["4xl", "5xl"]}
+            fontWeight={"semibold"}
+            color="blackAlpha.400"
+          >
+            Hello, I'm
+          </Text>
+          <AnimatePresence initial={false} mode="wait">
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={variants}
+              key={position}
+            >
+              <Text fontSize={["4xl", "5xl"]} color="black" ml={isLineBreak ? 0 : 4}>
+                {dynamicText[position]}
+              </Text>
+            </motion.div>
+          </AnimatePresence>
+        </Flex>
+      </Flex>
+      <Divider />
     </>
   );
 };
